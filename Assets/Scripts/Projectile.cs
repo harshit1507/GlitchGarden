d935ba38f272bff0ce6a42dbc5f0558a023f0cef
harshit1507 <harshit1507@yahoo.com>
2021-04-14 22:48:57 +0530

    implement health

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float speed = 1f;
+    [SerializeField] float damage = 50f;
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var health = collision.GetComponent<Health>();
+        health.DealDamage(damage);
+    }
+}
